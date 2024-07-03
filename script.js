2ef9de2edfe7388f7e87e1bfabf1b70cf88831a4
Janina86 <janinabolba@gmail.com>
2024-07-03 16:34:29 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let clickCounter = 0;
+const button = document.getElementById("colorButton");
+function changeColor() {
+  clickCounter++;
+  if (clickCounter == 1) {
+    button.classList.remove("btn-danger");
+    button.classList.add("btn-warning");
+  } else if (clickCounter == 2) {
+    button.classList.remove("btn-warning");
+    button.classList.add("btn-success");
+  } else {
+    button.classList.remove("btn-success");
+    button.classList.add("btn-danger");
+    clickCounter = 0;
+  }
+}
